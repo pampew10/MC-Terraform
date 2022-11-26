cc823989f86565db0bcd2c0dcfddd1d73b25bd3f
pampew10 <47994490+pampew10@users.noreply.github.com>
2022-11-26 12:10:37 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,6 +44,10 @@ resource "google_compute_instance" "apache" {
 
   network_interface {
     network = "default"
+
+    access_config {
+      nat_ip = google_compute_address.static.address
+    }
   }
 
   metadata_startup_script = file("startup_script.sh")
