b6b192e026f7e7bd924d534aae23360dc9e00619
pampew10 <47994490+pampew10@users.noreply.github.com>
2022-11-26 12:25:46 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -67,5 +67,5 @@ resource "google_compute_firewall" "allow_http" {
 }
 
 output "public_ip_address" {
-  value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
+  value = "google_compute_instance.default.network_interface.0.access_config.0.nat_ip"
 }
