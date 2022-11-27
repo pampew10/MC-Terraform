63da27469863dcc0130d596fd9409c30afc60122
pampew10 <47994490+pampew10@users.noreply.github.com>
2022-11-27 11:16:48 -0800

    Update startup_script.sh

--- startup_script.sh
+++ startup_script.sh
@@ -1,5 +1,20 @@
 #! /bin/bash
-apt update
-apt -y install apache2
-cat <<EOF > /var/www/html/index.html
-<html><body><p>&#127790; &#127790; &#127790;</p></body></html>
+
+sudo apt update
+sudo apt -y install openjdk-8-jre-headless
+
+sudo useradd -r -m -U -d /opt/minecraft -s /bin/bash minecraft
+
+sudo su - minecraft
+
+mkdir -p ~/{backups,tools,server}
+
+wget https://launcher.mojang.com/v1/objects/ed76d597a44c5266be2a7fcd77a8270f1f0bc118/server.jar -P ~/server
+
+cd ~/server
+
+java -Xmx1024M -Xms512M -jar server.jar nogui
+wait
+echo "eula=True" > eula.txt
+wait
+java -Xmx1024M -Xms512M -jar server.jar nogui
